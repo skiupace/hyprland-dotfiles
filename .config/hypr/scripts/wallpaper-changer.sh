0ae1f3f851d21bc3692af138af026c154afb9f1f
skiupace <ahmedalhakemee200335@gmail.com>
2024-08-22 18:33:49 +0300

    changed the wallpaper in hyprland.conf

--- .config/hypr/scripts/wallpaper-changer.sh
+++ .config/hypr/scripts/wallpaper-changer.sh
@@ -3,6 +3,9 @@
 # Directory containing wallpapers
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
 
+# Path to the Hyprland configuration file
+HYPRLAND_CONF="$HOME/.config/hypr/hyprland.conf"
+
 # Generate a list of wallpapers
 wallpapers=$(find "$WALLPAPER_DIR" -type f \( -name '*.jpg' -o -name '*.png' \) -print)
 
@@ -12,13 +15,14 @@ filenames=$(echo "$wallpapers" | xargs -n 1 basename)
 # Use rofi to select a wallpaper by filename
 selected_filename=$(echo "$filenames" | rofi -dmenu -i -p "Select Wallpaper:")
 
-# Find the full path corresponding to the selected filename
-selected_wallpaper=$(echo "$wallpapers" | grep "$(basename "$selected_filename")")
-
 # Check if a wallpaper was selected
-if [ -n "$selected_wallpaper" ]; then
-    # Set the wallpaper using swaybg
-    swaybg -i "$selected_wallpaper" -m fill
+if [ -n "$selected_filename" ]; then
+    # Set the path using $WALLPAPER_DIR
+    wallpaper_path="\$HOME/Pictures/Wallpapers/$selected_filename"
+    
+    # Update the $wallpaper variable in the hyprland.conf file
+    sed -i "s|^\(\s*\$wallpaper\s*=\s*\).*|\1$wallpaper_path|" "$HYPRLAND_CONF"
+    echo "Wallpaper has been updated in hyprland.conf"
 else
     echo "No wallpaper selected."
 fi
