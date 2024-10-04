23ceede0d610692cfb9d4bb810fae08b27849d21
skiupace <ahmedalhakemee200335@gmail.com>
2024-10-05 00:33:44 +0300

    edited some hyprland & waybar configs

--- .config/nvim/lua/plugins/nvim-tmux-navigator.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "christoomey/vim-tmux-navigator",
-    vim.keymap.set('n', 'C-h', ':TemuxNavigateLeft<CR>'),
-    vim.keymap.set('n', 'C-j', ':TemuxNavigateDown<CR>'),
-    vim.keymap.set('n', 'C-k', ':TemuxNavigateUp<CR>'),
-    vim.keymap.set('n', 'C-l', ':TemuxNavigateRight<CR>')
-}
